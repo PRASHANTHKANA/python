d77411ba2d8cfa1f8510924b5c823898ecc9c89d
PRASHANTHKANA <prashanthkanna404@gmail.com>
2024-11-12 17:45:35 +0530

    Add files via upload

---
+++ A,BC,D.E PATTERN 81.py
@@ -0,0 +1,12 @@
+for r in range(65,70):
+    for c in range(65,70):
+        if c<=r:
+            print(chr(c),end=' ')
+    print()
+for r in range(69,64,-1):
+    for c in range(65,70):
+        if c<=r:
+            print(chr(c),end=' ')
+    print()
+print()
+print()
